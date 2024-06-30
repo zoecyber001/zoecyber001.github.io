f00c7be7825ac3a60acbe349fbc210ebce27b95f
Olaoluwa Aina <110189878+zoecyber001@users.noreply.github.com>
2024-06-30 20:31:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// script.js
+
+document.getElementById("theme-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+    const mainContent = document.querySelector('.main-content');
+    mainContent.classList.toggle("light-mode");
+    mainContent.classList.toggle("dark-mode");
+    const footer = document.querySelector('.footer');
+    footer.classList.toggle("light-mode");
+    footer.classList.toggle("dark-mode");
+    const Youtube = document.querySelector('.youtube');
+    Youtube.classList.toggle("light-mode");
+    Youtube.classList.toggle("dark-mode");
+  });
+  
